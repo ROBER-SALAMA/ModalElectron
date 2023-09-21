ef83a630f21cdebf95aad9dffad30a5e6b511f5b
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-09-21 10:04:28 -0600

    😊

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import Modal from './components/global/Modal.vue';
+  // import Dasboard from './components/global/Dasboard.vue';
+</script>
+
+<template>
+  <div>
+    <Dasboard/>
+    <Modal></Modal>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/global/Modal.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center">
+    <button type="button" @click="openModal"
+      class="rounded-md bg-blue-600 bg-opacity-80 px-4 py-2 text-sm font-medium text-dark hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+      Crear
+    </button>
+  </div>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+        leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div class="flex min-h-full items-center justify-center p-4 text-center">
+          <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95">
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <DialogTitle as="h3" class="text-center font-medium leading-6 text-gray-900">
+                Crear rol
+              </DialogTitle>
+              <div class="mt-2">
+                <input as="input" type="text" name="#" placeholder="Ingrese un rol"
+                  class="mt-2 p-2 block w-full rounded-md bg-white border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+              </div>
+
+              <div class="mt-4 flex justify-center">
+                <button type="button"
+                  class="rounded-md border border-transparent bg-green-500 px-4 py-2 text-sm font-medium text-dark hover:bg-green-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 mx-1"
+                  @click="closeModal"
+                  >
+                  Crear
+                </button>
+                <button type="button"
+                  class="rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-dark hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 mx-1"
+                  @click="closeModal"
+                  >
+                  Cancelar
+                </button>
+              </div>
+
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
+
+const isOpen = ref(false);
+
+function closeModal() {
+  isOpen.value = false;
+}
+function openModal() {
+  isOpen.value = true;
+}
+</script>
